5e3673abac7939797231245ebf6c276105f704a5
AlexisECE <63599911+AlexisECE@users.noreply.github.com>
2020-04-19 17:59:57 +0200

    Add files via upload

--- inscription.php
+++ inscription.php
@@ -1,85 +1,47 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>Login</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1"> 
-        <link rel="stylesheet" type="text/css" href="inscription.css">
-        <link rel="stylesheet"
-            href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    </head> 
-    <body> 
-        <form action="inscriptions.php" method="post">
-        <div class="container">
+<head>
+<link rel="stylesheet" href="test1.css" />
+</head>
+<body>
+<?php
+require('config.php');
+if (isset($_REQUEST['username'], $_REQUEST['email'], $_REQUEST['password'])){
+  // récupérer le nom d'utilisateur et supprimer les antislashes ajoutés par le formulaire
+  $username = stripslashes($_REQUEST['username']);
+  $username = mysqli_real_escape_string($conn, $username); 
+  // récupérer l'email et supprimer les antislashes ajoutés par le formulaire
+  $email = stripslashes($_REQUEST['email']);
+  $email = mysqli_real_escape_string($conn, $email);
+  // récupérer le mot de passe et supprimer les antislashes ajoutés par le formulaire
+  $password = stripslashes($_REQUEST['password']);
+  $password = mysqli_real_escape_string($conn, $password);
+  //requéte SQL + mot de passe crypté
+    $query = "INSERT into `users` (username, email, password)
+              VALUES ('$username', '$email', '".hash('sha256', $password)."')";
+  // Exécuter la requête sur la base de données
+    $res = mysqli_query($conn, $query);
+    if($res){
+       echo "<div class='sucess'>
+             <h3>Vous êtes inscrit avec succès.</h3>
+             <p>Cliquez ici pour vous <a href='login.php'>connecter</a></p>
+       </div>";
+    }
+}else{
+?>
+<div class="container">
             <nav class="navbar navbar-expand">
-              <a class="navbar-brand" href="index.html"><img src="poirelog.png" alt="logo" width="150px" height="90px"/></a>
-              <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
-                <span class="navbar-toggler-icon"></span>
+              <a class="navbar-brand" href="index.php"><img src="poirelog.png" alt="logo" width="150px" height="90px"/></a>
                </button>
             </nav> 
-            
-
-            <div class="row">
-                <div class="col-md-5 mx-auto">
-                <div id="first">
-                    <div class="myform form">
-                         <div class="logo mb-3">
-                             <div class="col-md-12 text-center">
-                                <h1><b>Créer un compte</b></h1>
-                                <h8>Deja membre? <a href= login.html ><u>Se connecter</u> </a></h8>
-                                <br><br><br>
-                                <form action="inscriptions.php" method="post">
-                                    <table>
-                                         <tr>
-                                            
-                                            <td><input type="text" name="prenom" placeholder="Prénom" size="38px"></td> 
-                                        </tr>
-                                        <tr>
-                                            <td><input type="text" name="nom" placeholder="Nom" size="38px"></td> 
-                                        </tr>
-                                        <tr>
-                                            <td><input type="email" name="email" placeholder="Adresse e-mail" size="38px"></td> 
-                                        </tr>
-                                       
-                                        <tr>
-                                            <td><input type="password" name="mdp" placeholder="Mot de passe" size="38px"></td> 
-                                        </tr> 
-
-                                        <tr>
-                                            <td><small><br>En cliquant sur <b>S'inscrire</b>, vous confirmez avoir lu et accepté les <a href= condition.html ><u>Conditions d'utilisation</u> </a>. Veuillez vous référer à notre <a href= avis.html ><u>Avis sur les données personnelles</u> </a> pour toute information concernant le traitement de vos données.</small>
-                                                <br><br></td>
-                                        </tr>
-
-                                        <tr>
-
-                                        <td colspan="2" align="center">
-        <input type="submit" name="button" value="S'inscrire"></td>
-
-                                        </tr>
-                                        </table>
-
-
-                                </form>
-                                
-                              </div>
-
-                              </div>
-                            </form>
-                             
-                              <footer class="page-footer"> 
-                            
-                                    <div class="footer-copyright text-center">&copy; <small>Copyright © 1995-2020 Poire Inc. Tous droits réservés. <a href= condition.html ><u>Conditions d'utilisation</u> </a>,<a href= donneeperso.html ><u>Données personnelles</u> </a>,<a href= cookie.html ><u>cookies</u> </a> et <a href= adchoice.html ><u>AdChoice</u> </a></small>
-                                    </div>
-                                    </footer>   
-
-            <script>
-              $('a1').mouseover(function() {
-                $(this).dropdown('toggle')
-              })
-            </script>
-
-    </body>
+<form class="box" action="" method="post">
+  <h1 class="box-logo box-title">Créer un compte</h1>
+    <p class="box-register">Déjà membre? <a href= login.php ><u>Se connecter</u> </a></p>
+  <input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur" required />
+    <input type="text" class="box-input" name="email" placeholder="Email" required />
+    <input type="password" class="box-input" name="password" placeholder="Mot de passe" required />
+    <input type="submit" name="submit" value="S'inscrire" class="box-button" />
+</form>
+<?php } ?>
+</body>
 </html>
-

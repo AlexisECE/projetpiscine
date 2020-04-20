a2a8904b78bf0f77853f83daf0ba80b4ea38a528
AlexisECE <63599911+AlexisECE@users.noreply.github.com>
2020-04-20 23:54:19 +0200

    ALEXIS & PA

--- accessoirevip.php
+++ accessoirevip.php
@@ -56,121 +56,51 @@
                   <a class="dropdown-item" href="logout.php">Déconnexion</a>
                 </div>
 </div>
+<li class="nav-item"><a class="nav-link" href="encherir.php">Enchère</a></li>
             <li class="nav-item"><a class="nav-link" href="admin.php">Admin</a></li>
-            <li class="nav-item"><a class="nav-link" href="help.html">Aide</a></li>
+            
             </ul> </div>
           </div>
             <br>
           <div class="container">
           <h4>CATEGORIE : ACCESSOIRE VIP</h4>
 
-          <br>
-              <div class="row">
-      
-                <div class="col-lg-4 col-md-6 mb-4">
-                  <div class="card h-100">
-                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                    <div class="card-body">
-                      <h4 class="card-title">
-                        <a href="#">Item One</a>
-                      </h4>
-                      <h5>$24.99</h5>
-                      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                    </div>
-                    <div class="card-footer">
-                      <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                    </div>
-                  </div>
-                </div>
-      
-                <div class="col-lg-4 col-md-6 mb-4">
-                  <div class="card h-100">
-                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                    <div class="card-body">
-                      <h4 class="card-title">
-                        <a href="#">Item Two</a>
-                      </h4>
-                      <h5>$24.99</h5>
-                      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-                    </div>
-                    <div class="card-footer">
-                      <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                    </div>
-                  </div>
-                </div>
-      
-                <div class="col-lg-4 col-md-6 mb-4">
-                  <div class="card h-100">
-                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                    <div class="card-body">
-                      <h4 class="card-title">
-                        <a href="#">Item Three</a>
-                      </h4>
-                      <h5>$24.99</h5>
-                      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                    </div>
-                    <div class="card-footer">
-                      <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                    </div>
-                  </div>
-                </div>
-      
-                <div class="col-lg-4 col-md-6 mb-4">
-                  <div class="card h-100">
-                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                    <div class="card-body">
-                      <h4 class="card-title">
-                        <a href="#">Item Four</a>
-                      </h4>
-                      <h5>$24.99</h5>
-                      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                    </div>
-                    <div class="card-footer">
-                      <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                    </div>
-                  </div>
-                </div>
-      
-                <div class="col-lg-4 col-md-6 mb-4">
-                  <div class="card h-100">
-                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                    <div class="card-body">
-                      <h4 class="card-title">
-                        <a href="#">Item Five</a>
-                      </h4>
-                      <h5>$24.99</h5>
-                      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-                    </div>
-                    <div class="card-footer">
-                      <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                    </div>
-                  </div>
-                </div>
-      
-                <div class="col-lg-4 col-md-6 mb-4">
-                  <div class="card h-100">
-                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                    <div class="card-body">
-                      <h4 class="card-title">
-                        <a href="#">Item Six</a>
-                      </h4>
-                      <h5>$24.99</h5>
-                      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                    </div>
-                    <div class="card-footer">
-                      <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                    </div>
-                  </div>
-                </div>
-      
-              </div>
-              <!-- /.row -->
-      
-            </div>
-            <!-- /.col-lg-9 -->
-      
-          </div>
-          <!-- /.row -->
+          <?php
+	//identifier votre base de données (BDD)
+	$database = "poire";
+	
+	//connectez-vous dans votre BDD
+	$db_handle = mysqli_connect('localhost', 'root', 'root' );
+	$db_found = mysqli_select_db($db_handle, $database);
+	
+	//si le BDD existe, faire le traitement
+	if ($db_found) {
+    //sans condition
+    
+		$sql = "SELECT * FROM items WHERE Categorie LIKE 'Accessoire VIP'";
+		$result = mysqli_query($db_handle, $sql);
+		while ($data = mysqli_fetch_assoc($result)) {
+      echo "<br>";
+      echo "Categorie: " . $data['Categorie'] . "<br>";
+      echo "Nom: " . $data['Nom'] . "<br>";
+      echo "Descriptions: " . $data['Descriptions'] . "<br>";
+      echo "Prixmin: " . $data['Prixmin'] . "<br>";
+      echo "Prixmax: " . $data['Prixmax'] . "<br>";
+      $image = $data['Couverture'];
+      echo "Photo" . "<img src='$image' height='120' width='100'>" .
+"</td>";
+ 
+			
+		}//end while
+
+	}//end if
+	//si le BDD n'existe pas
+	else {
+		echo "Database not found";
+	}//end else
+	//fermer la connection
+	mysqli_close($db_handle);
+?>
       
         </div>
         <!-- /.container -->

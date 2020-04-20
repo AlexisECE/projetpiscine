693fcfb809953bcb3156c6a83e7eb3e361e65fb6
AlexisECE <63599911+AlexisECE@users.noreply.github.com>
2020-04-20 19:20:32 +0200

    ALEXIS & PA

--- ferraille.php
+++ ferraille.php
@@ -13,7 +13,7 @@
         <title>Ferraille ou Trésor</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1"> 
-        <link rel="stylesheet" type="text/css" href="categorie.css">
+        <link rel="stylesheet" type="text/css" href="style2.css">
         <link rel="stylesheet"
             href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
@@ -48,7 +48,14 @@
           <div class="collapse navbar-collapse" id="main-navigation">
             <ul class="navbar-nav">
             <li class="nav-item"><a class="nav-link" href="sell.php">Vendre</a></li>
-            <li class="nav-item"><a class="nav-link" href="login.php">Votre compte</a></li> 
+             <div class="dropdown">
+            <a1 class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Mon compte</a1>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="panier.php">Mon panier</a>
+                  <a class="dropdown-item" href="annonce.php">Mes annonces</a>
+                  <a class="dropdown-item" href="logout.php">Déconnexion</a>
+                </div>
+</div>
             <li class="nav-item"><a class="nav-link" href="admin.php">Admin</a></li>
             <li class="nav-item"><a class="nav-link" href="help.html">Aide</a></li>
             </ul> </div>
@@ -171,14 +178,13 @@
 
             </nav> 
 
-            <footer class="page-footer"> 
-              <div class="footer-copyright text-center">&copy; <small>Copyright © 1995-2020 Poire Inc. Tous droits réservés. <a href= condition.html ><u>Conditions d'utilisation</u> </a>,<a href= donneeperso.html ><u>Données personnelles</u> </a>,<a href= cookie.html ><u>cookies</u> </a> et <a href= adchoice.html ><u>AdChoice</u> </a></small> </div>
-              </footer>  
+
 
             <script>
               $('a1').mouseover(function() {
                 $(this).dropdown('toggle')
               })
             </script>
+
     </body>
 </html>

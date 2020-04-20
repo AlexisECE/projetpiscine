eb0404094ce8a8a4334a9f99b473d6601c24089d
AlexisECE <63599911+AlexisECE@users.noreply.github.com>
2020-04-20 18:10:23 +0200

    PA

--- items.sql
+++ items.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Apr 16, 2020 at 09:31 PM
+-- Generation Time: Apr 20, 2020 at 04:09 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
@@ -22,11 +22,12 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `items` (
   `ID` int(11) NOT NULL,
+  `Categorie` varchar(255) NOT NULL,
   `Nom` varchar(255) NOT NULL,
-  `Descriptions` varchar(255) NOT NULL,
-  `Prixmin` int(11) NOT NULL,
-  `Prixmax` int(11) NOT NULL,
-  `Photo` varchar(255) NOT NULL
+  `Descriptions` text NOT NULL,
+  `Prixmin` double NOT NULL,
+  `Prixmax` double NOT NULL,
+  `Image` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --

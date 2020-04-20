220298d142a4104a0a65bdea6af45ffdee414511
AlexisECE <63599911+AlexisECE@users.noreply.github.com>
2020-04-20 19:22:35 +0200

    ALEXIS

--- index2.php
+++ index2.php
@@ -1,4 +1,13 @@
 <!DOCTYPE html> 
+<?php
+  // Initialiser la session
+  session_start();
+  // Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
+  if(!isset($_SESSION["username"])){
+    header("Location: login.php");
+    exit(); 
+  }
+?>
 <html>
     <head>
         <title>Index</title>
@@ -13,7 +22,7 @@
     <body> 
         <div class="container">
             <nav class="navbar navbar-expand">
-              <a class="navbar-brand" href="index.php"><img src="poire2.png" alt="logo" width="120px" height="60px"/></a>
+              <a class="navbar-brand" href="index2.php"><img src="poire2.png" alt="logo" width="120px" height="60px"/></a>
               <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
                 <span class="navbar-toggler-icon"></span>
                </button>
@@ -22,168 +31,59 @@
               <div class="dropdown">
                 <a1 class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Catégories</a1>
                 <div class="dropdown-menu">
-                  <a class="dropdown-item" href="ferraille.html">Ferraille ou Trésor</a>
-                  <a class="dropdown-item" href="bpmusee.html">Bon pour le Musée</a>
-                  <a class="dropdown-item" href="accessoirevip.html">Accessoire VIP</a>
+                  <a class="dropdown-item" href="ferraille.php">Ferraille ou Trésor</a>
+                  <a class="dropdown-item" href="bpmusee.php">Bon pour le Musée</a>
+                  <a class="dropdown-item" href="accessoirevip.php">Accessoire VIP</a>
                 </div>
               </div>
 
               
-            <form class="form-inline">
+            <form class="form-inline" action="recherche.php">
                 <input class="form-control " type="search" placeholder="Rechercher un item" aria-label="Search" size="63px">
-                <button class="btn btn-outline-light" type="submit">Rechercher</button>
+                <button class="btn btn-outline-light" name="button1" type="submit">Rechercher</button>
               </form>
               
           <div class="collapse navbar-collapse" id="main-navigation">
             <ul class="navbar-nav">
-            <li class="nav-item"><a class="nav-link" href="sell.html">Vendre</a></li>
+            <li class="nav-item"><a class="nav-link" href="sell.php">Vendre</a></li>
             <div class="dropdown">
             <a1 class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Mon compte</a1>
                 <div class="dropdown-menu">
-                  <a class="dropdown-item" href="ferraille.html">blabla</a>
-                  <a class="dropdown-item" href="infoperso.html">Mes informations personnelles</a>
+                  <a class="dropdown-item" href="panier.php">Mon panier</a>
+                  <a class="dropdown-item" href="annonce.php">Mes annonces</a>
                   <a class="dropdown-item" href="logout.php">Déconnexion</a>
                 </div>
 </div>
-            <li class="nav-item"><a class="nav-link" href="admin.html">Admin</a></li>
+            <li class="nav-item"><a class="nav-link" href="admin.php">Admin</a></li>
             <li class="nav-item"><a class="nav-link" href="help.html">Aide</a></li>
             </ul> 
           </div>
           </div> 
 
-
+<br>
 
           <div class="container">
-            <div class="row">
-  
-        
-        
-              <div class="col-lg-12">
-        
-                <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-                  <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                  </ol>
-                  <div class="carousel-inner" role="listbox">
-                    <div class="carousel-item active">
-                      <img class="d-block img-fluid" src="covid.jpg" alt="First slide">
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block img-fluid" src="banniere.jpg" alt="Second slide">
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block img-fluid" src="fond1.png" alt="Third slide">
-                    </div>
-                  </div>
-                  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                  </a>
-                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                  </a>
-                </div>
-        
+          
                 <div class="row">
         
-                  <div class="col-lg-4 col-md-6 mb-4">
+                  <div class="col-lg-12">
                     <div class="card h-100">
-                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                      <div class="card-body">
-                        <h4 class="card-title">
-                          <a href="#">Item One</a>
-                        </h4>
-                        <h5>$24.99</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                      </div>
-                      <div class="card-footer">
-                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                      </div>
+                      <a href="ferraille.php"><img class="card-img-top" src="fer.png" alt=""></a>
                     </div>
                   </div>
         
-                  <div class="col-lg-4 col-md-6 mb-4">
+                  <div class="col-lg-12">
                     <div class="card h-100">
-                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                      <div class="card-body">
-                        <h4 class="card-title">
-                          <a href="#">Item Two</a>
-                        </h4>
-                        <h5>$24.99</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-                      </div>
-                      <div class="card-footer">
-                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                      </div>
+                      <a href="bpmusee.php"><img class="card-img-top" src="bpm.png" alt=""></a>
                     </div>
                   </div>
         
-                  <div class="col-lg-4 col-md-6 mb-4">
+                  <div class="col-lg-12">
                     <div class="card h-100">
-                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                      <div class="card-body">
-                        <h4 class="card-title">
-                          <a href="#">Item Three</a>
-                        </h4>
-                        <h5>$24.99</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                      </div>
-                      <div class="card-footer">
-                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                      </div>
+                      <a href="accessoirevip.php"><img class="card-img-top" src="vip.png" alt=""></a>
+                      
                     </div>
-                  </div>
-        
-                  <div class="col-lg-4 col-md-6 mb-4">
-                    <div class="card h-100">
-                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                      <div class="card-body">
-                        <h4 class="card-title">
-                          <a href="#">Item Four</a>
-                        </h4>
-                        <h5>$24.99</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                      </div>
-                      <div class="card-footer">
-                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                      </div>
-                    </div>
-                  </div>
-        
-                  <div class="col-lg-4 col-md-6 mb-4">
-                    <div class="card h-100">
-                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                      <div class="card-body">
-                        <h4 class="card-title">
-                          <a href="#">Item Five</a>
-                        </h4>
-                        <h5>$24.99</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-                      </div>
-                      <div class="card-footer">
-                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                      </div>
-                    </div>
-                  </div>
-        
-                  <div class="col-lg-4 col-md-6 mb-4">
-                    <div class="card h-100">
-                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                      <div class="card-body">
-                        <h4 class="card-title">
-                          <a href="#">Item Six</a>
-                        </h4>
-                        <h5>$24.99</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                      </div>
-                      <div class="card-footer">
-                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                      </div>
-                    </div>
-                  </div>
+                </div>
         
                 </div>
                 <!-- /.row -->
@@ -197,20 +97,8 @@
           </div>
           <!-- /.container -->
           </nav> 
-
-
-
-      
-         
-
           
-          <footer class="page-footer"> 
-            <div class="footer-copyright text-center">&copy; <small>Copyright © 1995-2020 Poire Inc. Tous droits réservés. <a href= condition.html ><u>Conditions d'utilisation</u> </a>,<a href= donneeperso.html ><u>Données personnelles</u> </a>,<a href= cookie.html ><u>cookies</u> </a> et <a href= adchoice.html ><u>AdChoice</u> </a></small> </div>
-            </footer> 
-           
-
-
-            
+          
            <script>
               $('a1').mouseover(function() {
                 $(this).dropdown('toggle')

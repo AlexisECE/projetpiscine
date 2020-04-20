1ba925be9e096eb9ce08158074f6c0f6adc40a13
AlexisECE <63599911+AlexisECE@users.noreply.github.com>
2020-04-20 19:23:58 +0200

    CORENTIN

--- panier.php
+++ panier.php
@@ -10,6 +10,7 @@
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     </head> 
+    
     <body> 
         <div class="container">
             <nav class="navbar navbar-expand">

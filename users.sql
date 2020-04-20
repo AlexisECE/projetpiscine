845b4e1cf9387eaaea072bbf0ac5c596876f6da6
AlexisECE <63599911+AlexisECE@users.noreply.github.com>
2020-04-20 18:00:58 +0200

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,9 +1,9 @@
- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump
 -- version 4.9.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Apr 19, 2020 at 04:27 PM
+-- Generation Time: Apr 20, 2020 at 04:00 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
@@ -24,8 +24,19 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `type` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `type`, `password`) VALUES
+(1, 'alexis_luu', 'alexis.luu@edu.ece.fr', 'admin', 'aa3d2fe4f6d301dbd6b8fb2d2fddfb7aeebf3bec53ffff4b39a0967afa88c609'),
+(2, 'pa_rebuffi', 'pierre.rebuffi@hotmail.fr', 'admin', 'aa3d2fe4f6d301dbd6b8fb2d2fddfb7aeebf3bec53ffff4b39a0967afa88c609'),
+(3, 'papa', 'papa@hotmail.fr', '', '58cc6181518fbfb49da9c50db08ce31aa92feebaecc6c46a3a8084e87b50f037'),
+(4, 'mama', 'mama@hotmail.fr', 'user', 'ccb711f092ac8ef1805b5045fab7e8a6189cb97ad04565e21b5fbcfc9e542e42');
 
 --
 -- Indexes for dumped tables
@@ -45,4 +56,4 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
